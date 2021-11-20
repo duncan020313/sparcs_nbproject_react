ec70357ce904afd5003c2877e7de71ce8c5c54cb
dongjaelee <milkbear0313@gmail.com>
2021-11-21 00:57:23 +0900

    Add room make form

--- src/components/mkroom/Mkroom.js
+++ src/components/mkroom/Mkroom.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Mkroom = () => {
+    const [maxMember, setMaxMember] = useState(3);
+    const incMaxMember = () => {
+        setMaxMember(maxMember+1);
+    }
+    const decMaxMember = () => {
+        setMaxMember(maxMember-1);
+    }
     return(
         <div className="mkroom">
-            mkroom
+            <form>
+                <textarea defaultValue="Roomname"></textarea>
+                <div className="counter">
+                    <button type="button" onClick={incMaxMember}>+</button>
+                    {maxMember}
+                    <button type="button" onClick={decMaxMember}>-</button>
+                </div>
+                <textarea defaultValue="eating house name"></textarea>
+                <div>
+                    <button className="submitbutton" type="submit">Submit</button>
+                </div>   
+            </form>
+                
         </div>
     )
 }
